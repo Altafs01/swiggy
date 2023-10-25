fd17b62a992d50c263180e503d3d5ea48307fc38
Altafs01 <altafshaikh1697@gmail.com>
2023-10-25 23:30:50 +0530

    changes i have made

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+// var phrases = ["Cooking gone wrong ?", "Unexpected guest ?" ,"Hungry ?" ,"Movie marathon ?" , "Game night ?"];
+// var currentPhraseIndex = 0;
+// var phraseElement = document.getElementById('phrase');
 
+// function changeText() {
+//     phraseElement.innerHTML = phrases[currentPhraseIndex];
+//     currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+// }
+// setInterval(changeText, 3000);
 
